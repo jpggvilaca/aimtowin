2fac5024fed7f1be6471b59bd2fe0f1f6af5f663
João Vilaça <jvilaca@rocketfuel.com>
2017-01-31 22:14:57 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+AimToWin eSports website
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <article>
+        &copy; 2017 AimToWin
+      </article>
+    </footer>
+  )
+}
+
+export default Footer
